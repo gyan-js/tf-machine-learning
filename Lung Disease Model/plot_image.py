daddc7f0c284673fdc8d0976a44d88861a25fbb4
Gyan <gyan0304@gmail.com>
2023-02-25 07:56:52 -0500

    FEAT: function to plot with image with prediction percent ✨\

---
+++ Lung Disease Model/plot_image.py
@@ -0,0 +1,25 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_image(i, predictions, true_label, test_images, train_images ):
+    true_label, predictions = true_label[i], predictions[i]
+    class_names = list(train_images.class_indices.keys())
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    batch = test_images.next()
+    image = batch[0][0].astype('uint8')
+    plt.imshow(image)
+
+    predicted_label = np.argmax(predictions)
+
+    if predicted_label == true_label:
+        color = 'red'
+    else:
+        color = 'green'
+
+    plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],100*np.max(predictions), "."), color=color)
+
+
+sys.modules[__name__] = plot_image
